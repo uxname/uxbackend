4843852982113ae166eff730d81e200c6c7cd4be
uxname <uxname@gmail.com>
2018-11-14 20:52:08 +0300

    Updated libraries

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -3,8 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var prisma_lib_1 = require("prisma-client-lib");
 var typeDefs = require("./prisma-schema").typeDefs;
 
+var models = [
+  {
+    name: "ActivationCode",
+    embedded: false
+  },
+  {
+    name: "Category",
+    embedded: false
+  },
+  {
+    name: "Product",
+    embedded: false
+  },
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "UserRole",
+    embedded: false
+  }
+];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
+  models,
   endpoint: `${process.env["PRISMA_ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
