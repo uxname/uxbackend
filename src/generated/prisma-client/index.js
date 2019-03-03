0c605cbcb953ee2de049861ab7a14bd5c4835944
uxname <uxname@gmail.com>
2019-03-03 15:05:46 +0300

    Added Prisma security & authentication

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -28,6 +28,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
+  secret: `${process.env["PRISMA_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
